ef272f06de48cdbd2e31ffbcf202ef7ad8cd733c
NdiayeDoucoure <doucoure.papa10@gmail.com>
2024-12-04 15:14:18 +0000

    Database setup and create employee entity for doing CRUD

---
+++ src/Entity/Employee.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EmployeeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EmployeeRepository::class)]
+class Employee
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $fName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $address = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $phoneNumber = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFName(): ?string
+    {
+        return $this->fName;
+    }
+
+    public function setFName(string $fName): static
+    {
+        $this->fName = $fName;
+
+        return $this;
+    }
+
+    public function getLName(): ?string
+    {
+        return $this->lName;
+    }
+
+    public function setLName(string $lName): static
+    {
+        $this->lName = $lName;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): static
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?string
+    {
+        return $this->phoneNumber;
+    }
+
+    public function setPhoneNumber(string $phoneNumber): static
+    {
+        $this->phoneNumber = $phoneNumber;
+
+        return $this;
+    }
+}
